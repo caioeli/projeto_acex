1ad202416fa4b05c7b44a095761599ba2b1f7f14
caio elias da silva <84875502+caioeli@users.noreply.github.com>
2025-05-16 20:00:30 -0300

    ultimas atualizaçoes

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/style.css?v=1.0">
+    <link rel="stylesheet" href="reset.css?v=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -10,28 +11,31 @@
 </head>
 <body>
 
+
 <header>
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #cbbbff;">
-  <div class="container-fluid">
-    <img src="../img/logo.png" alt="Logo do site" width="50" height="50" class="logo">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/projeto_acex/view/home.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#topicos">Tópicos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Ajuda</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(168, 110, 207);">
+    <div class="container-fluid">
+    
+        <img src="../img/logo.png" alt="Logo do site" style="margin-right: 10px; width: 100px; height: 45px;">
+
+       <button class="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div  style="margin-left:1150px;" class="collapse navbar-collapse d-flex justify-content-center align-items-center" id="navbarText">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link"  style="color:white;" href="/projeto_acex/view/home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"  style="color:white;"  href="">Tópicos</a>
+          </li>
+          <li class="nav-item">
+            <a  class="nav-link" style="color:white;" href="#">Ajuda</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </header>
    
 
---
+++ view/cardSurdez.php
@@ -0,0 +1,72 @@
+
+<?php include '../includes/header.php'; ?> 
+<link rel="stylesheet" href="../css/style.css">
+
+<div class="container-sm" style="margin-top:100px" >
+    <div class="row justify-content-center text-center mt-5 mb-5">
+        <div class="col-sm-12">
+            <h1 aria-label="Bem-vindo ao Comunica+" >Bem-vindo ao Comunica+</h1>
+            <br>
+            <h3 aria-label="Surdez" >
+                Surdez
+            </h3>
+        </div>
+    </div>
+
+    <div class="conteinerInfo" style="margin-top: 50px; margin-bottom: 200px ;">
+
+        <iframe  class="infoVideo"  src="https://www.youtube.com/embed/fOVBoN_3ILA?si=psVZhMCPfPizOdvh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
+            <h2 style="margin:auto;  margin-bottom: 50px; margin-top: 50px;" >Surdez: Desafios e Caminhos para a Inclusão</h2>
+
+            <p style="font-size: 18px;">  
+                A surdez é uma condição que afeta milhões de brasileiros, impactando diretamente sua comunicação, acesso à informação e inclusão social. Apesar de avanços legislativos e tecnológicos, pessoas surdas ainda enfrentam diversos desafios em diferentes aspectos da vida.
+
+                A principal dificuldade para pessoas surdas é a comunicação com ouvintes. A maioria da população não domina a Língua Brasileira de Sinais (Libras), dificultando interações cotidianas. Além disso, muitos surdos têm o português como segunda língua, o que pode tornar a comunicação escrita um desafio adicional.
+
+                A falta de profissionais capacitados em Libras nas escolas e a escassez de materiais didáticos acessíveis comprometem a qualidade da educação para estudantes surdos. Isso pode resultar em defasagens no aprendizado e limitar oportunidades futuras.
+
+                Apesar de leis que incentivam a inclusão de pessoas com deficiência no mercado de trabalho, muitos surdos ainda enfrentam preconceito e falta de acessibilidade nos ambientes profissionais. A falta de oportunidades e o capacitismo são barreiras significativas.
+
+                A comunicação entre profissionais de saúde e pacientes surdos é frequentemente prejudicada pela ausência de intérpretes de Libras, levando a diagnósticos imprecisos e tratamentos inadequados.
+
+                A falta de legendas em filmes, ausência de intérpretes em eventos públicos e a escassez de conteúdos acessíveis limitam a participação de pessoas surdas em atividades culturais e sociais, contribuindo para o isolamento e a exclusão.
+
+                Investir no aprendizado da Língua Brasileira de Sinais é uma forma eficaz de promover a inclusão. 
+            </p>    
+
+            <p style="font-size: 18px;">   
+                Cursos presenciais e online estão disponíveis e podem capacitar ouvintes a se comunicarem de forma mais eficiente com surdos.
+
+                Ao se comunicar com uma pessoa surda, utilize gestos, expressões faciais e escrita para complementar a mensagem. Manter o contato visual e garantir que o ambiente esteja bem iluminado facilita a leitura labial e a compreensão.
+
+                Gritar não facilita a comunicação e pode ser interpretado como agressividade. Fale de forma clara, pausada e natural, sem exagerar na articulação das palavras.
+
+                Incentive a implementação de legendas em vídeos, a presença de intérpretes em eventos e a disponibilização de materiais acessíveis em locais públicos. Essas medidas ampliam o acesso à informação e à cultura para pessoas surdas.
+                Promova campanhas de sensibilização sobre a surdez e a importância da inclusão. Educar a sociedade sobre as necessidades e direitos das pessoas surdas é fundamental para combater o preconceito e promover a igualdade.
+                Incentive o uso de tecnologias que auxiliem na comunicação, como aplicativos de tradução de Libras, dispositivos de alerta visual e aparelhos auditivos, quando apropriado.
+                Reconheça e valorize a cultura surda, que possui sua própria língua, valores e tradições. 
+            </p>
+
+            <p style="font-size: 18px;">
+                Respeitar essa identidade é essencial para promover uma inclusão genuína e significativa.
+                Promover a inclusão de pessoas surdas é uma responsabilidade coletiva que exige empatia, conhecimento e ações concretas. Ao compreender os desafios enfrentados por essa comunidade e adotar medidas para superá-los, contribuímos para uma sociedade mais justa e acessível para todos.
+                Cursos presenciais e online estão disponíveis e podem capacitar ouvintes a se comunicarem de forma mais eficiente com surdos.
+                Ao se comunicar com uma pessoa surda, utilize gestos, expressões faciais e escrita para complementar a mensagem. Manter o contato visual e garantir que o ambiente esteja bem iluminado facilita a leitura labial e a compreensão.
+                Gritar não facilita a comunicação e pode ser interpretado como agressividade. Fale de forma clara, pausada e natural, sem exagerar na articulação das palavras.
+               
+        </p>
+
+            <br>
+            <br>
+        <p style="font-size: 30px;">
+            Obrigado por ver !
+        </p>
+    </div>
+      
+
+</div>
+
+
+
+<?php include '../includes/footer.php'; ?> 
