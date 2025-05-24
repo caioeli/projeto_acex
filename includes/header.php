93a5eda544c7713523cef06a068fdfb08b823ea2
caio elias da silva <84875502+caioeli@users.noreply.github.com>
2025-05-24 09:07:54 -0300

    atualização do home

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="reset.css?v=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -24,7 +25,7 @@
       <div  style="margin-left:1150px;" class="collapse navbar-collapse d-flex justify-content-center align-items-center" id="navbarText">
         <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link"  style="color:white;" href="/projeto_acex/view/home.php">Home</a>
+            <a class="nav-link"  style="color:white;" href="/projeto_acex/view/home.php">Homeeee</a>
           </li>
           <li class="nav-item">
             <a class="nav-link"  style="color:white;"  href="">Tópicos</a>
